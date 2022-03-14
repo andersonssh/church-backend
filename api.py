6c7d07cab1dc2159fdec1769c3e1b1efc1b745fd
andersonssh <4nd3r50n4r4uj0@gmail.com>
2022-03-14 16:43:51 -0300

    Adiciona rota de ranking

--- api.py
+++ api.py
@@ -1,7 +1,6 @@
 from flask import Flask
-
+from src.dbv.ranking import ranking_bp
 
 app = Flask(__name__)
 
-
-
+app.register_blueprint(ranking_bp)
--- src/dbv/ranking.py
+++ src/dbv/ranking.py
@@ -3,13 +3,22 @@ Ranking dos desbravadores
 """
 from flask import Blueprint
 from src.utils import sort_by_index
+from src.database import fetch
+from src.http import HTTP_OK
 
-ranking_bp = Blueprint('ranking_bp', __name__)
+ranking_bp = Blueprint('ranking_bp', __name__, url_prefix='/ranking')
 
 
-@ranking_bp.route('/api/ranking_dbv')
+@ranking_bp.route('/')
 def get_ranking():
-    """GET ranking dos dbvs baseado nos pontos
+    """GET /ranking : ranking dos dbvs baseado nos pontos
     """
-    sort_by_index()
-    return {}
+    members = fetch('members')
+
+    data = [
+        [member['name'], member['points']] for member in members
+    ]
+
+    ranked_by_points = sort_by_index(data, index=1, reverse=True)
+
+    return {'data': ranked_by_points}, HTTP_OK
