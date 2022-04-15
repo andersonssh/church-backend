08db97903535a9b76be04751419387d661238f1f
andersonssh <4nd3r50n4r4uj0@gmail.com>
2022-04-15 07:38:56 -0300

    Adiciona CORS

--- api.py
+++ api.py
@@ -2,11 +2,13 @@
 Arquivo principal da api
 """
 from flask import Flask
+from flask_cors import CORS
 from src.blueprints.dbv.ranking import ranking_bp
 from src.blueprints.dbv.members import members_bp
 from src.blueprints.auth import auth_bp
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(ranking_bp)
 app.register_blueprint(members_bp)
