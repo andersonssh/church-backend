becf8f952c97e2be57f4760050fc0520ae964520
andersonssh <4nd3r50n4r4uj0@gmail.com>
2022-04-22 18:10:59 -0300

    Adiciona verificacao de quantidade de membros que esta sendo retornada para rota GET /members

--- tests/integration/test_get_ranking.py
+++ tests/integration/test_get_ranking.py
@@ -9,8 +9,15 @@ def test_success_ranking_get(client, mock_member):
     """
     test de sucesso para a rota /ranking
     """
+    mock_member['_id'] = '1'
+    database.insert_document('members', mock_member)
+    mock_member['_id'] = '2'
+    database.insert_document('members', mock_member)
+    mock_member['_id'] = '3'
+    database.insert_document('members', mock_member)
+    mock_member['_id'] = '4'
     database.insert_document('members', mock_member)
 
-    response = client.get('/ranking/')
-
+    response = client.get('/ranking')
     assert response.status_code == HTTP_OK
+    assert len(response.json) == 4
