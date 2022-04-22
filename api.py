4383114e33e11c8666fab4ac1fc5df219c77aed4
andersonssh <4nd3r50n4r4uj0@gmail.com>
2022-04-22 15:59:24 -0300

    Adiciona rota para o quiz

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 from src.blueprints.dbv.ranking import ranking_bp
 from src.blueprints.dbv.members import members_bp
 from src.blueprints.auth import auth_bp
+from src.blueprints.church.quiz import quiz_bp
 
 app = Flask(__name__)
 CORS(app)
@@ -13,3 +14,4 @@ CORS(app)
 app.register_blueprint(ranking_bp)
 app.register_blueprint(members_bp)
 app.register_blueprint(auth_bp)
+app.register_blueprint(quiz_bp)
---
+++ src/blueprints/church/quiz.py
@@ -0,0 +1,46 @@
+from flask import Blueprint, request, jsonify
+from src.http import HTTP_BAD_REQUEST
+from src import database
+
+MAX_LIMIT_QUESTIONS = 15
+
+quiz_bp = Blueprint('quiz_bp', __name__)
+
+
+def _get_quiz_questions(level=0, matter='all', limit=10):
+    query = {}
+    if level != 0:
+        query['level'] = level
+    if matter != 'all':
+        query['matter'] = 'matter'
+
+    match = {'$match': query}
+    return database.db.get_collection('quiz').aggregate([match,
+                                                         {'$addFields': {'n_random': {'$rand': {}}}},
+                                                         {'$sort': {'n_random': 1}},
+                                                         {'$unset': ['_id', 'n_random', 'created_at', 'updated_at']},
+                                                         {'$limit': limit}])
+
+
+@quiz_bp.route('/quiz')
+def quiz():
+    level = request.args.get('level', '0')
+    matter = request.args.get('matter', 'all')
+    limit = request.args.get('limit', '10')
+    level_max = 3
+    matter_alloweds = ['all']
+
+    if level.isnumeric() and 0 <= int(level) <= level_max:
+        level = int(level)
+    else:
+        return {'message': f'level deve ser um numero entre 0 e {level_max}'}, HTTP_BAD_REQUEST
+
+    if matter not in matter_alloweds:
+        return {'message': f'Materias permitidas: {matter_alloweds}'}, HTTP_BAD_REQUEST
+
+    if limit.isnumeric() and 0 <= int(limit) <= MAX_LIMIT_QUESTIONS:
+        limit = int(limit)
+    else:
+        return {'message': f'Limite max: {MAX_LIMIT_QUESTIONS}'}
+
+    return jsonify(list(_get_quiz_questions(level, matter, limit)))
